55add556734901f44582335bf6a575af08e198d4
soar004 <soar004@student.kristiania.no>
2024-02-15 16:26:16 +0100

    Made a checkbox for kommune and fixed some more css

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "ol/ol.css";
 import "./modules/application/application.css";
 
-import {MapApplication} from "./modules/application/mapApplication";
+import { MapApplication } from "./modules/application/mapApplication";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 
 root.render(<MapApplication />);
-
--- src/modules/application/mapApplication.tsx
+++ src/modules/application/mapApplication.tsx
@@ -1,29 +1,42 @@
-import {Map, View} from "ol";
+import { Map, View } from "ol";
 import TileLayer from "ol/layer/Tile";
-import {OSM} from "ol/source";
-import React, {MutableRefObject, useEffect, useRef} from "react";
-import {useGeographic} from "ol/proj";
+import { OSM } from "ol/source";
+import React, { MutableRefObject, useEffect, useRef } from "react";
+import { useGeographic } from "ol/proj";
 
-import "ol/ol.css"
+import "ol/ol.css";
 import "./application.css";
-
-
 
 useGeographic();
 
 const map = new Map({
-    layers: [
-        new TileLayer({source: new OSM()})
-    ],
-    view: new View({
-        center: [11, 59], zoom: 10
-    })
-})
+  layers: [new TileLayer({ source: new OSM() })],
+  view: new View({
+    center: [11, 59],
+    zoom: 10,
+  }),
+});
 
 export function MapApplication() {
-    const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
-    useEffect(() => {
-        map.setTarget(mapRef.current);
-    }, []);
-    return <div ref={mapRef}></div>;
+  const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
+  useEffect(() => {
+    map.setTarget(mapRef.current);
+  }, []);
+  return (
+    <>
+      <header>
+        <h1>Exercise 4 Application</h1>
+      </header>
+      <nav>
+        <div>
+            <label>
+          <input type="checkbox" id="menu-toggle" />
+          Show kommuner
+            </label>
+        </div>
+      </nav>
+
+      <div ref={mapRef}></div>
+    </>
+  );
 }
