a79667fbe3331f5eae662e5779edf89319f41188
soar004 <soar004@student.kristiania.no>
2024-02-15 16:06:17 +0100

    Fix some css

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React from 'react';
 import ReactDOM from "react-dom/client";
+import "ol/ol.css";
+import "./modules/application/application.css";
 
 import {MapApplication} from "./modules/application/mapApplication";
 
