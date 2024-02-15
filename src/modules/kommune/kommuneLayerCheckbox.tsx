bf3fc26d65ed115e7a57e99d5cafa7b4e2261b09
soar004 <soar004@student.kristiania.no>
2024-02-15 17:57:25 +0100

    Added kommunelayercheckbox

---
+++ src/modules/kommune/kommuneLayerCheckbox.tsx
@@ -0,0 +1,26 @@
+import {React, useContext, useEffect, useState} from "react";
+import {kommuneLayer} from "./kommuneLayer";
+
+
+export function KommuneLayerCheckbox() {
+    const setLayers = UseContext(MapContext);
+    const [checked, setChecked] = useState(false);
+
+    useEffect(() => {
+        if (checked) {
+            setLayers((old) => [...old, kommuneLayer]);
+        }
+        return () => {
+            setLayers((old) => old.filter((layer) => layer !== kommuneLayer));
+        };
+    }, [checked]);
+
+    return (
+        <div>
+            <label>
+                <input type="checkbox" checked={checked} onChange={(e) => setChecked(e.target.checked)} />
+                Show kommuner
+            </label>
+        </div>
+    );
+}
